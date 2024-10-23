dc1e3296ab5aa6b7dc567c3d37026df5b58f0063
Sinthujan Sitchsapashan <ssitchsa@paul-f4Ar2s3.clusters.42paris.fr>
2024-10-23 18:58:59 +0200

    start_parsing

---
+++ objects/exec/init_game.d
@@ -0,0 +1,11 @@
+objects/exec/init_game.o: sources/exec/init_game.c includes/cub3d.h \
+  includes/../libft/libft.h includes/../mlx/mlx.h \
+  includes/../mlx/mlx_int.h
+
+includes/cub3d.h:
+
+includes/../libft/libft.h:
+
+includes/../mlx/mlx.h:
+
+includes/../mlx/mlx_int.h:
---
+++ objects/main.d
@@ -0,0 +1,10 @@
+objects/main.o: sources/main.c includes/cub3d.h includes/../libft/libft.h \
+  includes/../mlx/mlx.h includes/../mlx/mlx_int.h
+
+includes/cub3d.h:
+
+includes/../libft/libft.h:
+
+includes/../mlx/mlx.h:
+
+includes/../mlx/mlx_int.h:
