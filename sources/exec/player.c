37a21b03dc109a9f121834a4bdadeb13bcc7cb17
SHIN <126964670+0xSHIN@users.noreply.github.com>
2024-10-23 02:19:14 +0200

    Update player.c

--- sources/exec/player.c
+++ sources/exec/player.c
@@ -1,7 +1,36 @@
 #include "cub3d.h"
 
-void init_player(t_player *player)
+void init_player(t_player *player, t_config *config)
 {
-    player->x = 0;
-    player->y = 0;
+    // j'ajoute 0.5 ici pour etre bien au centre de la case
+    player->x = config->player_x + 0.5;
+    player->y = config->player_y + 0.5;
+    if (config->player_orientation == 'N')
+    {
+        player->dirX = 0;
+        player->dirY = -1;
+        player->planeX = 0.66;
+        player->planeY = 0;
+    }
+    else if (config->player_orientation == 'S')
+    {
+        player->dirX = 0;
+        player->dirY = 1;
+        player->planeX = -0.66;
+        player->planeY = 0;
+    }
+    else if (config->player_orientation == 'E')
+    {
+        player->dirX = 1;
+        player->dirY = 0;
+        player->planeX = 0;
+        player->planeY = 0.66;
+    }
+    else if (config->player_orientation == 'W')
+    {
+        player->dirX = -1;
+        player->dirY = 0;
+        player->planeX = 0;
+        player->planeY = -0.66;
+    }
 }
