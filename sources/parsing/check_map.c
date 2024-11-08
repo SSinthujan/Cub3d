be3b487c426c1abba438f133d817fd88e3db92bb
Sinthujan Sitchsapashan <ssitchsa@bess-f2r5s1.clusters.42paris.fr>
2024-11-09 00:13:45 +0100

    continue

--- sources/parsing/check_map.c
+++ sources/parsing/check_map.c
@@ -6,15 +6,39 @@
 /*   By: ssitchsa <ssitchsa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 14:14:22 by ssitchsa          #+#    #+#             */
-/*   Updated: 2024/10/26 22:19:28 by ssitchsa         ###   ########.fr       */
+/*   Updated: 2024/11/09 00:13:32 by ssitchsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+/*
+	ETAPE 0
+	verifier nom de map si il se termine bien par .cub
+
+	ETAPE 1
+	ouvrir le fichier de la map
+	prendre les 6 premieres lignes non vides avec gnl
+	si moins de 6 lignes -----> erreur
+	check si il y a NO SO WE EA (pas d'ordre precis), pas de doublons --> erreur
+	check si le mpx file to img a fonctionner correctement si oui -> continue
+																non free tout et return 1
+	check si il y a F C (pas d'ordre precis), pas de doublons --> erreur
+	check si le rgb est composer de 3 nombres compris entre 0 et 255 separe par 2 virgules si oui
+		-> continue ETAPE 2
+																non free tout et return 1
+
+	ETAPE 2
+	prendre la map avec gnl et la stocker
+	gerer les espaces vides
+	check map fermer
+	check caractere valide : 1, 0, et N S E W pour le player mais pas de doublon
+
+*/
+
 int	check_namefile(char *str)
 {
-	int c;
+	int	c;
 
 	c = ft_strcmp(ft_strchr(str, '.'), ".cub");
 	if (c)
@@ -23,4 +47,29 @@ int	check_namefile(char *str)
 		return (c);
 	}
 	return (0);
+}
+
+char	check_config(char *config)
+{
+	int		fd;
+	int		i;
+	char	*str;
+	char	*line;
+
+	str = NULL;
+	i = 0;
+	fd = open(config, O_RDONLY);
+	if (fd == -1)
+		return (printf("Error\nMap non-existent\n"), NULL);
+	while (i < 6)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		str = ft_strjoin(str, line);
+		free(line);
+		if (!fonction qui check si une ligne est vide)
+	}
+	close(fd);
+	return (str);
 }
---
+++ sources/parsing/parsing.c
@@ -0,0 +1,162 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssitchsa <ssitchsa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 20:33:04 by ssitchsa          #+#    #+#             */
+/*   Updated: 2024/11/07 20:34:25 by ssitchsa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	check_extension(char *cub)
+{
+	if (ft_strstr(cub, ".cub"))
+		return (0);
+	return (1);
+}
+
+int	nb_ligne(char *file)
+{
+	int		fd;
+	int		i;
+	char	*ligne;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	i = 0;
+	while (1)
+	{
+		ligne = get_next_line(fd);
+		if (!ligne)
+			break ;
+		free(ligne);
+		i++;
+	}
+	close(fd);
+	return (i);
+}
+
+int	remove_space(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	return (0);
+}
+
+int	check_xpm(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i])
+		i++;
+	if (i < 5)
+		return (0);
+	i -= 4;
+	while (line[i])
+	{
+		if (line[i] != ".xpm"[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	get_path(t_cube *cube, char *line, char **way)
+{
+	int	x;
+	int	y;
+
+	if (!check_xpm(line))
+		return (0);
+	*way = mlx_xpm_file_to_image(cube->mlx, line, &x, &y);
+	if (!*way)
+		return (0);
+	return (1);
+}
+
+int	get_info(t_cube *cube, char *line)
+{
+	if (!ft_strncmp(line, "NO ", 3))
+	{
+		if (!get_path(cube, line, &cube->texture.north))
+			return (printf("Map error\n", 0));
+		return (1);
+	}
+	if (!ft_strncmp(line, "SO ", 3))
+	{
+		if (!get_path(cube, line, &cube->texture.south))
+			return (printf("Map error\n", 0));
+		return (1);
+	}
+	if (!ft_strncmp(line, "EA ", 3))
+	{
+		if (!get_path(cube, line, &cube->texture.east))
+			return (printf("Map error\n", 0));
+		return (1);
+	}
+	if (!ft_strncmp(line, "WE ", 3))
+	{
+		if (!get_path(cube, line, &cube->texture.west))
+			return (printf("Map error\n", 0));
+		return (1);
+	}
+	if (!ft_strncmp(line, "C ", 2))
+	{
+		if (!get_path(cube, line, &cube->texture.ceiling))
+			return (printf("Map error\n", 0));
+		return (1);
+	}
+	if (!ft_strncmp(line, "F ", 2))
+	{
+		if (!get_path(cube, line, &cube->texture.floor))
+			return (printf("Map error\n", 0));
+		return (1);
+	}
+	return (0);
+}
+
+int	read_cub(t_cube *cube)
+{
+	char	*line;
+	int		c;
+
+	c = 0;
+	// int		i;
+	// int		j;
+	if (!check_extension(cube->av[2]))
+		return (ft_putstr_fd("Error\nWrong file extension\n", 1), 0);
+	// i = 0;
+	// j = nb_ligne(cube->av[2]);
+	cube->fd = open(cube->av[2], O_RDONLY, 0664);
+	// cube->map = ft_calloc(j + 1, sizeof(char *));
+	while (1)
+	{
+		line = get_next_line(cube->fd);
+		c++;
+		if (!line)
+			break ;
+		if (!get_info(cube, line))
+			return (0);
+		if (c < 8)
+		{
+			free(line);
+			continue ;
+		}
+		if (strchr(line, '1') == 0)
+			break ;
+		free(line);
+		c++;
+	}
+	// if (!init_init(cube))
+	// 	return (0);
+	return (1);
+}
